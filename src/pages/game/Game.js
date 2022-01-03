4307c0878466c22a9741c2fddd67a28d61b5ef06
Haley Bubley <haleybubley@haleys-imac.lan>
2022-01-03 11:13:48 -0600

    add react-location

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import styles from "./Game.module.css"
+import React, {useState} from 'react';
+import Slots from '../../components/slots/Slots';
+import { GAME_OBJECTS_ARRAY } from '../../utils/constants';
+import { getRandomInt } from '../../utils/functions';
+import styles from "./Game.module.css";
 const Game = () => {
+    const [gameArray, setGameArray] = useState([])
+
+    const shuffleGameArray = () => {
+        const newGameArray = []
+        const randomIndices = []
+        for (let i = 0; i < 4; i++) {
+            let newInt = getRandomInt(5);
+            randomIndices.push(newInt)
+        }
+        randomIndices.map(i => newGameArray.push(GAME_OBJECTS_ARRAY[i]))
+        setGameArray(newGameArray);
+    }
+
     return (
         <div>
-
+            <Slots array={gameArray} />
+            <button onClick={shuffleGameArray}>Shuffle</button>
         </div>
     )
 }
