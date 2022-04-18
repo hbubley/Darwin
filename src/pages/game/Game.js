99de3a6f42d50108a6a19f39bdb37224c8855928
Haley Bubley <haleybubley@haleys-imac.lan>
2022-04-18 14:11:35 -0500

    added more outcomes

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -12,6 +12,8 @@ const Game = () => {
     const [character, setCharacter] = useState(null);
     const [tempCharacter, setTempCharacter] = useState(null);
     const [gameArray, setGameArray] = useState([]);
+    const [gameText, setGameText] = useState([""]);
+    const [hasLost, setHasLost] = useState(false);
     const characterOptionsArray = GAME_OBJECTS_ARRAY.filter(obj => obj.type === "animal")
     const handleTempCharacterSelect = (selectedCharacter) => {
         setTempCharacter(selectedCharacter);
@@ -34,6 +36,7 @@ const Game = () => {
     }
 
     const determineOutcome = (gameArray, selectedAction) => {
+        const statuses = []
         const predators = gameArray.filter((gameObject) => gameObject?.prey?.includes(character?.animal));
         const prey = gameArray.filter((gameObject) => gameObject?.predators?.includes(character?.animal));
         const sleep = gameArray.filter((gameObject) => gameObject?.resource === "cave");
@@ -41,17 +44,41 @@ const Game = () => {
         const updatedCharacter = { ...character }
         updatedCharacter.sleep = updatedCharacter.sleep - 5
         updatedCharacter.hunger = updatedCharacter.hunger - 5
-        if (predators.length > 1) {
+        if (predators.length > 2) {
             updatedCharacter.colony_size = updatedCharacter.colony_size - 1;
-        } else if (selectedAction === "sleep") {
-            updatedCharacter.sleep = updatedCharacter.sleep + (sleep.length * 5)
-        } else if (selectedAction === "hunt") {
-            updatedCharacter.hunger = updatedCharacter.hunger + (prey.length * 5)
-        } else if (selectedAction === "breed") {
-            updatedCharacter.colony_size = updatedCharacter.colony_size + (mates.length * updatedCharacter?.offspring)
+            statuses.push(`You were attacked while attempting to ${selectedAction}! Your colony has shrunk...`)
+        } else if (selectedAction === "sleep" && updatedCharacter.sleep < 100 && sleep.length > 0) {
+            updatedCharacter.sleep = updatedCharacter.sleep + (sleep.length * updatedCharacter?.regeneration?.sleep)
+            statuses.push(`Don't you look rested! Your sleep has increased by ${sleep.length * updatedCharacter?.regeneration?.sleep}`)
+        } else if (selectedAction === "hunt" && updatedCharacter.hunger < 100 && prey.length > 0) {
+            updatedCharacter.hunger = updatedCharacter.hunger + (prey.length * updatedCharacter?.regeneration?.hunger)
+            statuses.push(`Succesful hunt! Your hunger has increased by ${prey.length * updatedCharacter?.regeneration?.hunger}`)
+        } else if (selectedAction === "breed" && mates.length > 0) {
+            updatedCharacter.colony_size = updatedCharacter.colony_size + (mates.length * updatedCharacter?.regeneration?.offspring)
+            statuses.push(`What luck! Your colony size has increased by ${mates.length * updatedCharacter?.regeneration?.offspring}`)
+        } else {
+            statuses.push("Boring.... Nothing has happened")
+        }
+
+        if (updatedCharacter.sleep <= 0 || updatedCharacter.hunger <= 0) {
+            updatedCharacter.colony_size = updatedCharacter.colony_size - 1
+            if (updatedCharacter.sleep <= 0) {
+                updatedCharacter.sleep = updatedCharacter.base_stats.sleep
+                statuses.push("Lack of energy has caused one of your offspring to die")
+            }
+            if (updatedCharacter.hunger <= 0) {
+                updatedCharacter.hunger = updatedCharacter.base_stats.hunger
+                statuses.push("Lack of food has caused one of your offspring to die")
+            }
+        }
+
+        if (updatedCharacter.offspring < 0) {
+            setHasLost(true)
         }
         setCharacter(updatedCharacter)
+        setGameText(statuses)
     }
+
 
 
 
@@ -59,13 +86,16 @@ const Game = () => {
         <div className={styles.root}>
             <Title label={character ? "Shuffle" : "Please begin by selecting a character"} />
             <Slots array={character ? gameArray : characterOptionsArray} onClick={character ? null : handleTempCharacterSelect} highlightedCard={character ? null : tempCharacter} />
-            <div>
-                <CharacterStats
-                    hunger={character ? character.hunger : tempCharacter?.hunger}
-                    sleep={character ? character.sleep : tempCharacter?.sleep}
-                    colonySize={character ? character.colony_size : tempCharacter?.colony_size}
-                />
-            </div>
+            {
+                gameText.length > 0 && gameText.map(text => {
+                    return  <p>{text} </p> 
+                })
+            }
+            <CharacterStats
+                hunger={character ? character.hunger : tempCharacter?.hunger}
+                sleep={character ? character.sleep : tempCharacter?.sleep}
+                colonySize={character ? character.colony_size : tempCharacter?.colony_size}
+            />
             {character
                 ? <div>
                     <Button
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,36 +2,63 @@ export const GAME_OBJECTS_ARRAY = [{
     type: "animal",
     animal: "rabbit",
     image: "https://res.cloudinary.com/dum4u7sro/image/upload/v1641324322/Untitled%20design/3_tpxacd.png",
-    offspring: 5,
     sleep: 70,
     colony_size: 5,
     hunger: 50,
     predators: ["snake", "falcon"],
     prey: ["carrots"],
     difficulty: "hard",
+    regeneration:{
+        hunger: 8,
+        sleep: 10,
+        offspring: 5,
+    },
+    base_stats:{
+        sleep: 70,
+        colony_size: 5,
+        hunger: 50,
+    }
 }, {
     type: "animal",
     animal: "falcon",
-    offspring: 1,
     image: "https://res.cloudinary.com/dum4u7sro/image/upload/v1641324321/Untitled%20design/5_m3zeib.png",
     sleep: 30,
     colony_size: 1,
+    regeneration:{
+        hunger: 10,
+        sleep: 10,
+        offspring: 1,
+    },
     hunger: 30,
     predators: [],
     prey: ["rabbit", "snakes"],
-    difficulty: "easy"
+    difficulty: "easy",
+    base_stats: {
+        sleep: 30,
+        colony_size: 1,
+        hunger: 30,
+    }
 
 }, {
     type: "animal",
     animal: "snake",
     image: "https://res.cloudinary.com/dum4u7sro/image/upload/v1641324322/Untitled%20design/4_cvpisr.png",
-    offspring: 3,
     sleep: 30,
     colony_size: 1,
     hunger: 100,
     predators: ["falcon"],
     prey: ["rabbit"],
     difficulty: "medium",
+    regeneration:{
+        hunger: 20,
+        sleep: 5,
+        offspring: 3,
+    },
+    base_stats:{
+        sleep: 30,
+        colony_size: 1,
+        hunger: 100,
+    }
 }, {
     type: "resource",
     resource: "cave",
